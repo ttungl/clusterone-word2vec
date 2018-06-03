848bd20a337854dc67d3dffaa057d7f90f93b423
Tung Thanh Le <ttungl@users.noreply.github.com>
2018-06-03 15:21:47 -0500

    Add files via upload

--- main_tensorflow_w2v.py
+++ main_tensorflow_w2v.py
@@ -15,18 +15,18 @@ from sklearn.manifold import TSNE
 # load dataset
 url = 'http://mattmahoney.net/dc/'
 
-def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified %s' % filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-      'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
+# def maybe_download(filename, expected_bytes):
+#   """Download a file if not present, and make sure it's the right size."""
+#   if not os.path.exists(filename):
+#     filename, _ = urlretrieve(url + filename, filename)
+#   statinfo = os.stat(filename)
+#   if statinfo.st_size == expected_bytes:
+#     print('Found and verified %s' % filename)
+#   else:
+#     print(statinfo.st_size)
+#     raise Exception(
+#       'Failed to verify ' + filename + '. Can you get to it with a browser?')
+#   return filename
 
 
 
@@ -40,8 +40,9 @@ def read_data(filename):
   return data
 
 
-filename = maybe_download('text8.zip', 31344016)
-
+# filename = maybe_download('text8.zip', 31344016)
+# /data/user/clusterone_dataset_name/path
+filename = 'data/ttungl/word2vecdata/text8.zip'
 words = read_data(filename)
 
 ## exclude words in stoplist
